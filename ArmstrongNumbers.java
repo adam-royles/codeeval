909010d3e0a40425dd9a1066ae5dc574ee973b59
adam-royles <adam.royles@gmail.com>
2017-08-22 16:15:49 +0100

    Update ArmstrongNumbers.java

--- ArmstrongNumbers.java
+++ ArmstrongNumbers.java
@@ -5,20 +5,15 @@ public class ArmstrongNumbers {
 
     public static void main (String[] args) throws IOException {
 
-//                File file = new File(args[0]);
-                BufferedReader buffer = new BufferedReader(new FileReader("src/testing.txt"));
+                File file = new File(args[0]);
+                BufferedReader buffer = new BufferedReader(new FileReader(file));
                 String line;
                 while ((line = buffer.readLine()) != null) {
                     line = line.trim();
                     // Process line of input Here
                     String string = line;
-
-
-                   int rawData = Integer.parseInt(line);
-
-
+                    int rawData = Integer.parseInt(line);
                     int l = String.valueOf(rawData).length();
-
                     int total = 0;
                     int multi= 0;
                     String[] bits = string.split("");
@@ -29,10 +24,8 @@ public class ArmstrongNumbers {
 
                         for(int k=1; k<l-1; ++k){
                             multi = multi * n;
-
                         }
                         total += n*multi;
-
                     }
 
                     if(total == rawData){
