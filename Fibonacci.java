50a843b1f33e9d9bd70447049305faed808797bb
adam-royles <adam.royles@gmail.com>
2017-08-22 15:46:16 +0100

    Add Fibonacci challenge

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Fibonacci {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File(args[0]);
+        Scanner in = new Scanner(file);
+
+        while(in.hasNextLine()) {
+            int num = Integer.parseInt(in.nextLine());
+            System.out.println(solve(num));
+        }
+    }
+
+    // calculates and returns the given nths fibonacci number
+    public static int solve(int n) {
+        if (n <= 1)
+            return n;
+        else
+            return solve(n-1) + solve(n-2);
+    }
+}
